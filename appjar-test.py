ed0f80aa805e514a29c977604a856aaa5720a634
Ben <bstephens2278@gmail.com>
2018-07-31 20:10:36 -0400

    clean up

--- appjar-test.py
+++ appjar-test.py
@@ -4,33 +4,27 @@ import hashlib
 import uuid
 import sqlite3
 
-# vars
+# connect to database
 database_name = 'accounts.sqlite'
+conn = sqlite3.connect(database_name)
+c = conn.cursor()
+
+# make sure the database is set up
 table_accounts = 'Accounts'
 username_field = 'Usernames'
 salt_field = 'Salts'
 hash_field = 'PasswordHash'
 field_type = "TEXT"
-
-# connect to database
-conn = sqlite3.connect(database_name)
-c = conn.cursor()
-
-# make sure the database is set up
 colums = [i[1] for i in c.execute('PRAGMA table_info(' + table_accounts + ')')]
 if username_field not in colums:
     try:
-        c.execute('ALTER TABLE ' + table_accounts +
-                  ' ADD COLUMN ' + username_field + ' TEXT')
+        c.execute('ALTER TABLE ' + table_accounts + ' ADD COLUMN ' + username_field + ' TEXT')
     except:
-        c.execute('CREATE TABLE {tn} ({nf} {ft})'
-                  .format(tn=table_accounts, nf=username_field, ft=field_type))
+        c.execute('CREATE TABLE {tn} ({nf} {ft})' .format(tn=table_accounts, nf=username_field, ft=field_type))
 if salt_field not in colums:
-    c.execute('ALTER TABLE ' + table_accounts +
-              ' ADD COLUMN ' + salt_field + ' TEXT')
+    c.execute('ALTER TABLE ' + table_accounts + ' ADD COLUMN ' + salt_field + ' TEXT')
 if hash_field not in colums:
-    c.execute('ALTER TABLE ' + table_accounts +
-              ' ADD COLUMN ' + hash_field + ' TEXT')
+    c.execute('ALTER TABLE ' + table_accounts + ' ADD COLUMN ' + hash_field + ' TEXT')
 # ................................
 
 
@@ -38,13 +32,11 @@ def password_hasher(password, salt=""):
     if salt == "":
         salt = uuid.uuid4().hex
         print(salt)
-    return hashlib.sha512(salt.encode('utf-8') + password.encode(
-                          'utf-8')).hexdigest(), salt
+    return hashlib.sha512(salt.encode('utf-8') + password.encode('utf-8')).hexdigest(), salt
 
 
 def add_account(username, salt, hashed_password):
-    c.execute("INSERT INTO %s VALUES('%s', '%s', '%s')" %
-              (table_accounts, username, salt, hashed_password))
+    c.execute("INSERT INTO %s VALUES('%s', '%s', '%s')" % (table_accounts, username, salt, hashed_password))
     conn.commit()
 
 
@@ -67,8 +59,7 @@ def press(button):
         print(usr, salt, hashed_password)
         # hashed_password, salt = password_hasher(pwd, salt)
         # add_account(usr, salt, hashed_password)
-        # print("User:", usr, "Pass:", pwd,
-        #       "Hashed Password:", hashed_password)
+        # print("User:", usr, "Pass:", pwd, "Hashed Password:", hashed_password)
     else:
         print("Button " + button + " is not configured")
 
