c3f2e64f66c98abf0e4b246b819d45ad6bfc320d
Ben <bstephens2278@gmail.com>
2018-08-03 21:24:14 -0400

    .

--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -3,12 +3,11 @@ import pygame as pg
 import time
 
 def run_game():
-    def add_body(x,y):
-        snake_bodies.insert(0,[x, y, 10, 10])
-        #snake_bodies.append(snake_bodies[-1]
+    def add_body():
+        snake_bodies.append(snake_bodies[-1])
 
     def move(direction):
-        snake_bodies.insert(0,[snake_head_rect.x+2, snake_head_rect.y+2, 10, 10])
+        snake_bodies.insert(0,[snake_head_rect.x+3, snake_head_rect.y+3, 10, 10])
         del snake_bodies[-1]
         if direction == "RIGHT":
             snake_head_rect.x += 15
@@ -19,9 +18,12 @@ def run_game():
         elif direction == "DOWN":
             snake_head_rect.y += 15
 
+    def place_food():
+        pg.draw.circle(screen,(200,0,200),(30,30), 5)
+
 
     pg.init()
-    screen = pg.display.set_mode((1200, 800))
+    screen = pg.display.set_mode((1200, 750))
     pg.display.set_caption("Snake")
 
     bg_color = (230,230,230)
@@ -29,9 +31,8 @@ def run_game():
 
     #screen_rect = screen.get_rect()
 
-    snake_head_rect = pg.Rect(600, 400, 15, 15)
-    snake_bodies = []
-    add_body(snake_head_rect.x+2, snake_head_rect.y+2)
+    snake_head_rect = pg.Rect(600, 375, 15, 15)
+    snake_bodies = [(snake_head_rect.x+3, snake_head_rect.y+3,10,10)]
 
     color = (100,100,100)
     #pg.draw.rect(screen, color, snake_head_rect)
@@ -58,11 +59,20 @@ def run_game():
                     direction = "DOWN"
                 elif event.key == pg.K_q:
                     sys.exit()
+                elif event.key == pg.K_1:
+                    add_body()
+                elif event.key == pg.K_2:
+                    place_food()
 
         # Refresh
         pg.display.flip()
         screen.fill(bg_color)
         pg.draw.rect(screen, color, snake_head_rect)
+        pg.draw.circle(screen,(200,0,200),(8,8), 7)
+        for w in range(0,1200,15):
+            pg.draw.line(screen,(220,220,220),(w,0),(w,750),1)
+        for h in range(0,800,15):
+            pg.draw.line(screen,(220,220,220),(0,h),(1200,h),1)
         for body in snake_bodies:
             (x, y, w, h) = body
             pg.draw.rect(screen, color, pg.Rect(x, y, w, h))
